5238e39a95f55260dac64701d7782b536d2c2d9b
Daian Petre-Mihai <daian.pe.petre@student.utcluj.ro>
2025-01-23 20:09:07 +0200

    monitoring thread created with stopping feature

---
+++ monitoring_thread.py
@@ -0,0 +1,35 @@
+import threading
+import time
+from typing import Callable, Any
+
+
+class MonitoringThread(threading.Thread):
+    def __init__(self, target: Callable[..., Any], sleep_time: float = 1, *args, **kwargs):
+        """
+        Initialize the thread with a target function to run in a loop.
+
+        :param target: The function to execute repeatedly.
+        :param args: Positional arguments for the target function.
+        :param kwargs: Keyword arguments for the target function.
+        """
+        super().__init__()
+        # event to signal when to stop
+        self._stop_event = threading.Event()
+        self.target = target
+        self.args = args
+        self.kwargs = kwargs
+        self.delay = sleep_time
+
+    def run(self):
+        # check the stop signal
+        print("Monitoring thread started.")
+        while not self._stop_event.is_set():
+            self.target(*self.args, **self.kwargs)  # call the target function
+            time.sleep(self.delay)
+        print("Monitoring thread stopping...")
+
+    def stop(self):
+        """
+        Signal the thread to stop.
+        """
+        self._stop_event.set()
