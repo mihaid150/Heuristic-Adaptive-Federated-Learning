793c67af1d3a8a92362b69c1217edc60b6ddd4f5
Daian Petre-Mihai <daian.pe.petre@student.utcluj.ro>
2025-02-17 18:03:54 +0200

    updated edge resources paths

--- edge_node/edge_resources_paths.py
+++ edge_node/edge_resources_paths.py
@@ -6,9 +6,10 @@ class EdgeResourcesPaths(str, Enum):
     EDGE_MODEL_FILE_NAME = "edge_model.keras"
     RETRAINED_EDGE_MODEL_FILE_NAME = "retrained_edge_model.keras"
 
-    DATA_FOLDER_PATH = "/app/data"
-    INPUT_DATA_PATH = "lorem ipsum"
-    FILTERED_DATA_PATH = "lorem ipsum"
+    DATA_FOLDER_PATH = "/app/data/"
+    FILTERED_DATA_FOLDER_PATH = "/app/data/filtered_data/"
+    INPUT_DATA_PATH = DATA_FOLDER_PATH + "input_data.csv"
+    FILTERED_DATA_PATH = FILTERED_DATA_FOLDER_PATH + "filtered_data.csv"
 
-    CURRENT_DAY_DATA_PATH = "lorem ipsum"
-    NEXT_DAY_DATA_PATH = "lorem ipsum"
+    CURRENT_DAY_DATA_PATH = FILTERED_DATA_FOLDER_PATH + "current_day_data.csv"
+    NEXT_DAY_DATA_PATH = FILTERED_DATA_FOLDER_PATH + "next_day_data.csv"
