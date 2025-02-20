64aef85ab838fe3cd28aad3a633e308ced016ae7
Daian Petre-Mihai <daian.pe.petre@student.utcluj.ro>
2025-02-20 09:14:28 +0200

    created a path for saving genetic population

--- fog_node/fog_resources_paths.py
+++ fog_node/fog_resources_paths.py
@@ -6,3 +6,4 @@ class FogResourcesPaths(str, Enum):
     FOG_MODEL_FILE_NAME = "fog_model.keras"
     EDGE_MODEL_FILE_NAME = "edge_model.keras"
     LAMBDA_PREV_FILE_NAME = "lambda_prev.json"
+    GENETIC_POPULATION_FILE_NAME = "genetic_population.json"
