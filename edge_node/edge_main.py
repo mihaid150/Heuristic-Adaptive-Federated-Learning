b68bdce740d8c1cc0f96cf225c8f87791f922fd8
Daian Petre-Mihai <daian.pe.petre@student.utcluj.ro>
2025-03-16 17:14:29 +0200

    updated the work flow and added dynamic sequence length

--- edge_node/edge_main.py
+++ edge_node/edge_main.py
@@ -1,11 +1,38 @@
 # edge_node/edge_main.py
 from http.client import HTTPException
+from typing import Any, Dict
 
 from fastapi import APIRouter
+from starlette.websockets import WebSocket, WebSocketDisconnect
+from shared.logging_config import logger
 
 from edge_node.edge_service import EdgeService
 
 edge_router = APIRouter()
+
+
+@edge_router.websocket('/ws')
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            message: Dict[str, Any] = await websocket.receive_json()
+            operation: str = message['operation']
+            data: Dict[str, Any] = message['data']
+
+            try:
+                if operation == "configure_edge_data_parameters":
+                    response = configure_edge_data_parameters(data)
+                elif operation == "get_edge_data_parameters":
+                    response = get_edge_data_parameters()
+                else:
+                    response = {"error": "Invalid operation"}
+            except Exception as e:
+                response = {"error": str(e)}
+
+            await websocket.send_json(response)
+    except WebSocketDisconnect:
+        logger.warning("Websocket disconnected.")
 
 
 @edge_router.post("/execute-model-evaluation")
@@ -16,5 +43,13 @@ async def execute_model_evaluation_endpoint(message: dict):
     """
     result = EdgeService.execute_model_training_evaluation_http(message)
     if "error" in result:
-        raise HTTPException(status_code=400, detail=result["error"])
+        raise HTTPException()
     return result
+
+
+def configure_edge_data_parameters(data):
+    return EdgeService.set_edge_data_parameters(data)
+
+
+def get_edge_data_parameters():
+    return EdgeService.get_edge_training_parameters()
